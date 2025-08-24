01f184463a90af9977b360f7421326a243b93cf7
bekkhurramov524 <bekkhurramov.com>
2025-08-25 01:22:06 +0500

    js loyihasi

---
+++ harajatlar/main.js
@@ -0,0 +1,249 @@
+//  const d = 11000
+//  const e = 12000
+//
+// const a = 500 * d
+//  const b = 250 * d
+//  const c = 120 * e
+//
+//  const s = a + b + c
+//
+//  let p = Number(prompt("Pulni kiriting (so'mda):"))
+//
+//  if (p >= s) {
+//     console.log("Oq yo'l!")
+//  } else {
+//     console.log("Pul yetmaydi")
+//
+//  }
+// let hozrgiyil = 2025;
+// let yosh  = 17;
+//
+// let birthYear = hozrgiyil - yosh;
+//
+// console.log("Sizning tug'ilgan yilingiz: " + birthYear);
+
+// const age = Number(prompt("yoshingizni kiriting"));
+// const kun =age*365;
+// const hours =kun*24;
+// const minutes =hours*60;
+// console.log("siz taxminan"+ kun+"kun," +hours +"soat,"+minutes +"daqiqa yashagansz.");
+
+// const ism = prompt("ismingizni kiriting")
+// const familya =prompt("familyani kitiriting")
+// const name = ism + " " + familya
+//
+// console.log(name)
+
+
+// const bem = prompt("dollaringizni kiriting")
+// const dollar = (bem)
+// const kurs =  12400
+// const sum =dollar*kurs
+// console.log( "sizning summangiz "  +sum+" sum");
+
+//
+// const yoshi = prompt("yoshingizni kiriting")
+// let yosh;
+// if (yoshi === "bir") {
+//     yosh = 1
+// }
+// else if (yoshi === "ikki"){
+//     yosh = 2
+// }
+// else if (yoshi === "uch") {
+//     yosh = 3
+// }
+// else if (yoshi === "to'rt") {
+//     yosh = 4
+// }
+// else if (yoshi === "besh") {
+//     yosh = 5
+// }
+// else if (yoshi === "olti") {
+//     yosh = 6
+// }
+// else if (yoshi === "yetti") {
+//     yosh = 7
+// }
+// else if (yoshi === "sakkiz") {
+//     yosh = 8
+// }
+// else if (yoshi === "to'qqiz") {
+//     yosh = 9
+// }
+// else if (yoshi === "o'n") {
+//     yosh = 10
+// }
+// else if (yoshi === "o'n bir") {
+//     yosh = 11
+// }
+// else if (yoshi === "o'n ikki") {
+//     yosh =12
+// }
+// else if (yoshi === " o'n uch") {
+//     yosh = 13
+// }
+// else if (yoshi === "un turt") {
+//     yosh = 14
+// }
+// else if (yoshi === "o'n besh") {
+//     yosh = 15
+// }
+// else if (yoshi === "on olti") {
+//     yosh = 16
+// }
+// else if (yoshi === "o'n yetti") {
+//     yosh = 17
+// }
+// else if (yoshi === "o'n sakkiz") {
+//     yosh = 18
+// }
+// else if (yoshi === "o'n toqqiz") {
+//     yosh = 19
+// }
+// else if (yoshi === "yigirma") {
+//     yosh = 20
+// }
+// else if (yoshi === "yigirma bir") {
+//     yosh = 21
+// }
+// else if (yoshi === "uigirma ikki") {
+//     yosh = 22
+// }
+// if (yoshi === "yigirma uch") {
+//     yosh = 23;
+// } else if (yoshi === "yigirma to'rt") {
+//     yosh = 24;
+// } else if (yoshi === "yigirma besh") {
+//     yosh = 25;
+// } else if (yoshi === "yigirma olti") {
+//     yosh = 26;
+// } else if (yoshi === "yigirma yetti") {
+//     yosh = 27;
+// } else if (yoshi === "yigirma sakkiz") {
+//     yosh = 28;
+// } else if (yoshi === "yigirma to'qqiz") {
+//     yosh = 29;
+// } else if (yoshi === "o'ttiz") {
+//     yosh = 30;
+// } else if (yoshi === "o'ttiz bir") {
+//     yosh = 31;
+// } else if (yoshi === "o'ttiz ikki") {
+//     yosh = 32;
+// } else if (yoshi === "o'ttiz uch") {
+//     yosh = 33;
+// } else if (yoshi === "o'ttiz to'rt") {
+//     yosh = 34;
+// } else if (yoshi === "o'ttiz besh") {
+//     yosh = 35;
+// } else if (yoshi === "o'ttiz olti") {
+//     yosh = 36;
+// } else if (yoshi === "o'ttiz yetti") {
+//     yosh = 37;
+// } else if (yoshi === "o'ttiz sakkiz") {
+//     yosh = 38;
+// } else if (yoshi === "o'ttiz to'qqiz") {
+//     yosh = 39;
+// } else if (yoshi === "qirq") {
+//     yosh = 40;
+// } else if (yoshi === "qirq bir") {
+//     yosh = 41;
+// } else if (yoshi === "qirq ikki") {
+//     yosh = 42;
+// } else if (yoshi === "qirq uch") {
+//     yosh = 43;
+// } else if (yoshi === "qirq to'rt") {
+//     yosh = 44;
+// } else if (yoshi === "qirq besh") {
+//     yosh = 45;
+// } else if (yoshi === "qirq olti") {
+//     yosh = 46;
+// } else if (yoshi === "qirq yetti") {
+//     yosh = 47;
+// } else if (yoshi === "qirq sakkiz") {
+//     yosh = 48;
+// } else if (yoshi === "qirq to'qqiz") {
+//     yosh = 49;
+// } else if (yoshi === "ellik") {
+//     yosh = 50;
+// } else if (yoshi === "ellik bir") {
+//     yosh = 51;
+// } else if (yoshi === "ellik ikki") {
+//     yosh = 52;
+// } else if (yoshi === "ellik uch") {
+//     yosh = 53;
+// } else if (yoshi === "ellik to'rt") {
+//     yosh = 54;
+// } else if (yoshi === "ellik besh") {
+//     yosh = 55;
+// } else if (yoshi === "ellik olti") {
+//     yosh = 56;
+// } else if (yoshi === "ellik yetti") {
+//     yosh = 57;
+// } else if (yoshi === "ellik sakkiz") {
+//     yosh = 58;
+// } else if (yoshi === "ellik to'qqiz") {
+//     yosh = 59;
+// } else if (yoshi === "oltmish") {
+//     yosh = 60;
+// }
+//
+// console.log("sizning yoshingiz " +yosh)
+
+//
+// const yosh= +prompt("yoshingizni kiriting")
+// if(yosh>=18){
+//     console.log("sz voyaga yetkansz")
+// }
+// else {
+//     console.log("sz voyaga yetmagansz")
+// }
+// const son =+prompt("son kiriting")
+// if (son >0) {
+//     console.log("MUSBAT")
+//
+// }
+// else if (son < 0) {
+//     console.log("Manfiy")
+// }
+// else if (son === 0) {
+//     console.log("nol")
+
+
+
+// const parol = +prompt("parol kiriting")
+// if (parol ===123) {
+//     console.log(
+//         "hush kelibsz"
+//     )
+// }
+// else{
+//     console.log("parol hato!")
+// }
+
+
+// const baho     =prompt("balingizni kiriting")
+// if (baho >=90) {
+//     console.log("alo")
+// }
+// else if (baho>=70 && baho<=89) {
+//     console.log("yaxshi")
+// }
+// else if (baho >=50 && baho <=69) {
+//     console.log("qoniqarli")
+// }
+// else if (baho <50) {
+//     console.log("yomon")
+// }
+
+// const h = +prompt("Haroratni kiriting");
+//
+// if (h > 30) {
+//     console.log("Juda issiq, suv iching");
+// } else if (h >= 15) {
+//     console.log("Ob-havo yaxshi");
+// } else if (h >= 0) {
+//     console.log("Salqin, iliq kiying");
+// } else {
+//     console.log("Sovuq, qalin kiying");
+// }

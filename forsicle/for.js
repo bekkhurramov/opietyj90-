5885bd254383c5e3da6b4d288bc54ed83216d3bd
bekkhurramov524 <bekkhurramov.com>
2025-08-27 16:05:01 +0500

    mencswj

---
+++ forsicle/for.js
@@ -0,0 +1,50 @@
+// const a=12;
+// const b=7;
+// if (a<=b) {
+//     for (let i = a; i <= b; i++) {
+//         console.log(i);
+//     }
+//
+// }
+// else{
+//     for (let i = a; i >= b; i--) {
+//         console.log(i);
+//     }
+// }
+
+// const priceperk =12.5;
+// for (let i = 1; i <= 10; i++){
+//  const total=i*priceperk;
+//   console.log(i+"kg " +total +" so'm");
+//  }
+
+// const pricePerKg = 12.5;
+//
+// for (let i = 1; i <= 10; i++) {
+//     const weight = i / 10;
+//     const total = weight * pricePerKg;
+//     console.log(weight.toFixed(1) + " kg = " + total.toFixed(2) + " so'm");}
+
+// const a = +prompt("a:");
+// const b = +prompt("b:");
+// let yigindi = 0;
+//
+// for (let i = a; i <= b; i++) {
+//     yigindi += i;
+// }
+//
+// alert(yigindi);
+// const a = +prompt("a:");
+// const b = +prompt("b:");
+// let kopaytma = 1;
+//
+// for (let i = a; i <= b; i++) {
+//     kopaytma *= i;
+// }
+//
+// alert(kopaytma);
+// const matn = "ld22kje33herjbffnb234234fjhf34";
+//
+// const raqamlar = matn.replace(/\D/g, "");
+//
+// alert(raqamlar); // 223323423434
